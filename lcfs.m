446f2cce4d1c537d14ae721123dcb10312452240
Matteo Grandin <matteo.grandin@igi.cnr.it>
2025-05-18 19:31:42 +0200

    +

---
+++ lcfs.m
@@ -0,0 +1,14 @@
+clc; clear all; close all;
+
+shot = 85804; % TCV shot number
+
+mdsconnect('tcvdata.epfl.ch'); % Connect to the MDSplus server
+
+mdsopen('tcv_shot', shot);
+
+[t, ip] = tcvget('IPLIUQE'); % precalculated using liuqe
+
+t = t(1:13:end)
+
+[L,LX,LY] = liuqe(shot, t);
+
