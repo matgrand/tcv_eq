f332006d594a9d920e2386e7fe62a69a0f127ef3
Matteo Grandin <matteo.grandin@igi.cnr.it>
2025-05-26 18:37:19 +0200

    +b

--- create_ds.m
+++ create_ds.m
@@ -126,6 +126,7 @@ for i = 1:length(shots)
         assert(all(size(rq) == [129, nt]), 'rq has wrong size');
         assert(all(size(zq) == [129, nt]), 'zq has wrong size');
 
+        fprintf('\tBm0 size: %s, Bm1 size: %s\n', mat2str(size(Bm0)), mat2str(size(Bm1)));
         assert(all(size(Bm0) == size(Bm1) == [38, nt]), 'Bm has wrong size');
         assert(all(size(Ff0) == size(Ff1) == [38, nt]), 'Ff has wrong size');
         assert(all(size(Ft0) == size(Ft1) == [1, nt]), 'Ft has wrong size');
