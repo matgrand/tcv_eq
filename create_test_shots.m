362796a05be9337a67c4961b67b49ad54e32f18f
matgrand <matteo.grandin@gmail.com>
2025-07-30 15:38:25 +0200

    +

--- create_test_shots.m
+++ create_test_shots.m
@@ -24,10 +24,10 @@ mdsclose; % Close the MDSplus connection
 
 shots = [
     79742, % single null
-    86310, % double null
-    78893, % negative triangularity
-    83848, % ?
-    78071, % standard, test ctrl pts
+    % 86310, % double null
+    % 78893, % negative triangularity
+    % 83848, % ?
+    78071, % standard, test ctrl pts (t=0.571) (warn: theta is wrong)
 ];
 disp('Shots to process:');
 disp(shots);
@@ -51,14 +51,6 @@ for i = 1:length(shots)
         
         t = LY.t'; % time vector
         [t2, ip2] = tcvget('IP', t); % calculated using magnetics at liuqe times
-
-        % last closed flux surface (LCFS) (unfortunately it's not in mds2meq outputs (yet))
-        rq = mdsdata('tcv_eq("R_EDGE", "LIUQE.M", "NOEVAL")'); % LCFS r coordinate
-        zq = mdsdata('tcv_eq("Z_EDGE", "LIUQE.M", "NOEVAL")'); % LCFS z coordinate
-        theta = mdsdata('tcv_eq("THETA", "LIUQE.M", "NOEVAL")'); 
-        % check that theta is the same as theta0, first size, then values
-	    assert(all(size(theta) == size(theta0)), 'theta and theta0 have different sizes');
-        assert(all(abs(theta0 - theta) < 1e-6), 'theta and theta0 are different');
 
         % analyze the time vector
         assert(max(abs(t2 - t)) < 1e-8, 'Time vectors do not coincide');
